5819618f9642ebd7926e58dda83155c184b0277e
austin2118ace <austin2118ace@gmail.com>
2024-10-14 14:41:53 -0400

    temp main function

--- dewan_flir_camera/main.py
+++ dewan_flir_camera/main.py
@@ -1,5 +1,16 @@
+from _classes.cam import Cam
+from _classes.interface import Interface
+from _classes.spin_system import SpinSystem
+
 def main():
-    pass
+    with SpinSystem() as system:
+        for i, cam in enumerate(system.camera_list):
+            with Cam(cam, i) as camera:
+                print(camera)
+
+        del cam
+
+
 
 if __name__ == "__main__":
     main()
