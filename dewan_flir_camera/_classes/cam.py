0ea81deea9a514b7b2d9ac63d716814892c23e2f
austin2118ace <austin2118ace@gmail.com>
2024-10-14 14:11:09 -0400

    add private function to unwrap and exception into the __exit__ metho

--- dewan_flir_camera/_classes/cam.py
+++ dewan_flir_camera/_classes/cam.py
@@ -45,3 +45,6 @@ class Cam:
             return PySpin.CValuePtr(node).ToString()
         else:
             return None
+
+    def _exit_on_exception(self, ex):
+        self.__exit__(type(ex), str(ex), ex.__traceback__())
