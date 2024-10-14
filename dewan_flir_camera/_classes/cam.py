99ef8f8d77a1768115e79cbfdb695b90bc659f59
austin2118ace <austin2118ace@gmail.com>
2024-10-14 13:43:16 -0400

    Initial outline of camera class

---
+++ dewan_flir_camera/_classes/cam.py
@@ -0,0 +1,32 @@
+import PySpin
+
+class Cam:
+    def __init__(self, cam_ptr, number):
+        self.cam_ptr = cam_ptr
+        self.number = number
+        self.vendor = []
+        self.model = []
+
+        self._get_cam_info()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        import traceback
+        if exc_type is not None:
+            traceback.print_exception(exc_type, exc_val, exc_tb)
+        del self.cam_ptr
+        self.cam_ptr = []
+
+
+
+    def _get_cam_info(self):
+        if self.cam_ptr.TLDevice.DeviceVendorName.GetAccessMode() == PySpin.RO:
+            self.vendor = self.cam_ptr.TLDevice.DeviceVendorName.ToString()
+
+        if self.cam_ptr.TLDevice.DeviceModelName.GetAccessMode() == PySpin.RO:
+            self.model = self.cam_ptr.TLDevice.DeviceModelName.GetValue()
+
+    def __str__(self):
+        return f'Camera {self.number}, Vendor: {self.vendor}, Module: {self.model}'
