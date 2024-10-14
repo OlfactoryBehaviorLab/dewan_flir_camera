f283b271413b00a073f8fa475598f3fc632f7564
austin2118ace <austin2118ace@gmail.com>
2024-10-14 14:37:25 -0400

    Create SpinnakerObject class to hold all the generic functionality

---
+++ dewan_flir_camera/_classes/_generics.py
@@ -0,0 +1,27 @@
+import PySpin
+
+class SpinnakerObject:
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        import traceback
+        if exc_type is not None:
+            traceback.print_exception(exc_type, exc_val, exc_tb)
+
+    def __repr__(self):
+        return f'Class: {self.__class__}'
+
+    @staticmethod
+    def get_node_info(node):
+        if node is not None and PySpin.IsReadable(node):
+            return PySpin.CValuePtr(node).ToString()
+        else:
+            return None
+
+    @staticmethod
+    def _exit_on_exception(self, ex):
+        self.__exit__(type(ex), str(ex), ex.__traceback__())
