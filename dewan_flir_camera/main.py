ac15999744171a16ca01151e8f8d5fcedc46bc2b
austin2118ace <austin2118ace@gmail.com>
2024-10-16 19:15:04 -0400

    setup main loop to capture images

--- dewan_flir_camera/main.py
+++ dewan_flir_camera/main.py
@@ -1,16 +1,50 @@
-from _classes.cam import Cam
+import PySpin
+from PySpin import SpinnakerException
+from time import sleep
+
 from _classes.spin_system import SpinSystem
+from _classes.acquisition import ImageHandler
 
 def main():
+
     with SpinSystem() as system:
+        camera = system.cameras[0]
+        event_handler = ImageHandler('./images')
 
-        system.cameras[0].init()
+        camera.init()
 
-        system.cameras[0].configure_trigger()
-        print(system.cameras[0].ExposureTime.GetValue())
+        camera.configure_trigger()
+        camera.ExposureAuto.SetValue(PySpin.ExposureAuto_Continuous)
+        camera.configure_acquisition_mode(PySpin.AcquisitionMode_Continuous)
+        camera.register_event_handler(event_handler)
 
-        system.cameras[0].deinit()
+        while True:
+            try:
+                print('Waiting for trigger!')
+                camera.BeginAcquisition()
+                event_handler.reset()
+                wait_for_trigger(event_handler)
+                camera.EndAcquisition()
+            except KeyboardInterrupt:
+                camera.EndAcquisition()
+                break
 
+        camera.deinit()
+        del camera
+
+
+
+def wait_for_trigger(event_handler, num_frames=100, wait_time_s=0.1):
+    try:
+        while True:
+            sleep(wait_time_s)
+            if event_handler.num_acquired_images >= num_frames:
+                return
+    except SpinnakerException as se:
+        print('Error while acquiring images!')
+        print(se)
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
