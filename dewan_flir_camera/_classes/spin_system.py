8d6ae11e2004208c21290c7c867637b1e5776f29
austin2118ace <austin2118ace@gmail.com>
2024-10-15 20:06:47 -0400

    Add two public lists to hold camera and interface pointers for typechecking

--- dewan_flir_camera/_classes/spin_system.py
+++ dewan_flir_camera/_classes/spin_system.py
@@ -5,10 +5,14 @@ class SpinSystem(SpinnakerObject):
     def __init__(self):
         self.system : PySpin.System = []
         self.version = []
-        self.interface_list : PySpin.InterfaceList = []
+        self._interface_list : PySpin.InterfaceList = []
         self.num_interfaces : int = 0
-        self.camera_list : PySpin.CameraList = []
+        self._camera_list : PySpin.CameraList = []
         self.num_cams : int = 0
+
+        # Lists to hold the pointers so enumerate and for loops don't complain
+        self.cameras = []
+        self.interfaces = []
 
         self._initialize_system()
         super().__init__(self.system)
@@ -17,12 +21,13 @@ class SpinSystem(SpinnakerObject):
     def __exit__(self, exc_type, exc_val, tb):
         super().__exit__(exc_type, exc_val, tb)
 
-        self.camera_list.Clear()
-        self.interface_list.Clear()
-        self.system.ReleaseInstance()
-
         self.cameras = []
         self.interfaces = []
+
+        self._camera_list.Clear()
+        self._interface_list.Clear()
+        self.system.ReleaseInstance()
+
         self.system = []
 
 
@@ -30,10 +35,12 @@ class SpinSystem(SpinnakerObject):
         try:
             self.system = PySpin.System.GetInstance()
             self.version = self.system.GetLibraryVersion()
-            self.interface_list = self.system.GetInterfaces()
-            self.num_interfaces = self.interface_list.GetSize()
-            self.camera_list = self.system.GetCameras()
-            self.num_cams = self.camera_list.GetSize()
+            self._interface_list = self.system.GetInterfaces()
+            self.num_interfaces = self._interface_list.GetSize()
+            self.interfaces = list(self._interface_list)
+            self._camera_list = self.system.GetCameras()
+            self.num_cams = self._camera_list.GetSize()
+            self.cameras = list(self._camera_list)
 
             if self.num_cams == 0 or self.num_interfaces == 0:
                 print("No cameras present! Exiting!")
