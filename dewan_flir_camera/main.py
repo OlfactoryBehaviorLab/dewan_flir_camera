d116ef4bb011a5a391f803760b23b53e575ce483
austin2118ace <austin2118ace@gmail.com>
2024-10-15 23:03:45 -0400

    update main function

--- dewan_flir_camera/main.py
+++ dewan_flir_camera/main.py
@@ -1,15 +1,15 @@
 from _classes.cam import Cam
-from _classes.interface import Interface
 from _classes.spin_system import SpinSystem
 
 def main():
     with SpinSystem() as system:
-        for i, cam in enumerate(system.camera_list):
-            with Cam(cam, i) as camera:
-                print(camera)
 
-        del cam
+        system.cameras[0].init()
 
+        system.cameras[0].configure_trigger()
+        print(system.cameras[0].ExposureTime.GetValue())
+
+        system.cameras[0].deinit()
 
 
 if __name__ == "__main__":
