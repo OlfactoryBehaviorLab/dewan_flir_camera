e7a6287f0ac3ddeaf6c7a8d8795457ff3b0d33eb
austin2118ace <austin2118ace@gmail.com>
2024-10-18 18:09:46 -0400

    remove unused imports

--- dewan_flir_camera/_ui/FLIR.py
+++ dewan_flir_camera/_ui/FLIR.py
@@ -8,25 +8,20 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
-    QCursor, QFont, QFontDatabase, QGradient,
-    QIcon, QImage, QKeySequence, QLinearGradient,
-    QPainter, QPalette, QPixmap, QRadialGradient,
-    QTransform)
-from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QComboBox, QDoubleSpinBox,
-    QFrame, QGraphicsView, QGridLayout, QGroupBox,
-    QHBoxLayout, QLabel, QLayout, QMainWindow,
-    QMenu, QMenuBar, QPushButton, QSizePolicy,
-    QSpinBox, QStatusBar, QTabWidget, QVBoxLayout,
-    QWidget)
+from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect,
+                            QSize, Qt)
+from PySide6.QtGui import (QAction, QFont)
+from PySide6.QtWidgets import (QAbstractSpinBox, QComboBox, QDoubleSpinBox,
+                               QFrame, QGraphicsView, QGridLayout, QGroupBox,
+                               QHBoxLayout, QLabel, QLayout, QMenu, QMenuBar, QPushButton, QSizePolicy,
+                               QSpinBox, QStatusBar, QTabWidget, QVBoxLayout,
+                               QWidget)
 
 class MainUI(object):
     def __init__(self, main_ui):
         self.main_ui = main_ui
         self.about_widget = []
+
         self.setupUi(main_ui)
 
     def setupUi(self, main_window):
