11b42e6e97237f2f109fc695a9af2346c2360a7c
austin2118ace <austin2118ace@gmail.com>
2024-10-18 18:10:23 -0400

    main gui file launches UI

--- dewan_flir_camera/gui.py
+++ dewan_flir_camera/gui.py
@@ -1,9 +1,26 @@
-from ._ui import about, FLIR
-from PySide6.QtWidgets import QMainWindow
+from _ui import about, FLIR
+from PySide6.QtWidgets import QMainWindow, QApplication
 
 class ControlWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.main_ui = FLIR.MainUI(self)
-        self.main_ui.about_widget = about.Ui_about
+        self.main_ui.about_widget = about.About()
+        self.main_ui.about_widget.hide()
 
+
+def launch_gui():
+    app = QApplication.instance()
+    if not app:
+        app = QApplication([])
+
+    window = ControlWindow()
+    window.show()
+    window.main_ui.about_widget.show()
+    app.exec()
+
+
+
+
+if __name__ == "__main__":
+    launch_gui()
