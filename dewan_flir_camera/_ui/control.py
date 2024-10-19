46fbe7ad5e7146644dd458923d75a71af3886efd
austin2118ace <austin2118ace@gmail.com>
2024-10-18 23:55:28 -0400

    created callbacks

--- dewan_flir_camera/_ui/control.py
+++ dewan_flir_camera/_ui/control.py
@@ -8,5 +8,33 @@ class ControlWindow(QMainWindow):
         super().__init__()
         self.main_ui = FLIR.MainUI(self)
         self.main_ui.about_widget = about.About()
-
         self.main_ui.actionAbout.triggered.connect(self.main_ui.about_widget.show)
+        self.main_ui.actionOpen.triggered.connect(self.open_action_callback)
+        self.main_ui.actionSave.triggered.connect(self.save_action_callback)
+        self.main_ui.actionSave_As.triggered.connect(self.save_as_action_callback)
+
+        self.main_ui.start_button.clicked.connect(self.start_button_callback)
+        self.main_ui.stop_button.clicked.connect(self.stop_button_callback)
+        self.main_ui.arm_button.clicked.connect(self.arm_button_callback)
+        self.main_ui.capture_button.clicked.connect(self.capture_button_callback)
+
+    def start_button_callback(self):
+        pass
+
+    def stop_button_callback(self):
+        pass
+
+    def arm_button_callback(self):
+        pass
+
+    def capture_button_callback(self):
+        pass
+
+    def open_action_callback(self):
+        pass
+
+    def save_action_callback(self):
+        pass
+
+    def save_as_action_callback(self):
+        pass
