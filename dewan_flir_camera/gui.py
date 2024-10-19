29c31752aaa178f05cb4f4f128a7f3896a5d23c0
austin2118ace <austin2118ace@gmail.com>
2024-10-18 22:35:22 -0400

    force darkmode

--- dewan_flir_camera/gui.py
+++ dewan_flir_camera/gui.py
@@ -1,18 +1,20 @@
 from _ui.control import ControlWindow
 
 from PySide6.QtWidgets import QApplication
+import sys
+
 
 def launch_gui():
+    sys.argv += ['-platform', 'windows:darkmode=2']
+
     app = QApplication.instance()
     if not app:
-        app = QApplication([])
+        app = QApplication(sys.argv)
+    app.setStyle('fusion')
 
     window = ControlWindow()
     window.show()
-    window.main_ui.about_widget.show()
     app.exec()
-
-
 
 
 if __name__ == "__main__":
