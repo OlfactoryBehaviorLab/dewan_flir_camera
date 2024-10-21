15cd55ccf154a39b686c6129614f14602bcdd0b4
austin2118ace <austin2118ace@gmail.com>
2024-10-20 23:45:38 -0400

    update main to check for camera/interface once started some misc. test code

---
+++ dewan_flir_camera/main.py
@@ -0,0 +1,48 @@
+import PySpin
+from PySpin import SpinnakerException
+from time import sleep
+
+import gui
+from _classes.spin_system import SpinSystem
+from _classes.acquisition import ImageHandler
+
+
+def main():
+    gui.launch_gui(None)
+    with SpinSystem() as system:
+        system.get_interfaces()
+        system.get_cameras()
+
+        if system.num_cams == 0 or system.num_interfaces == 0:
+            print('No suitable cameras or interfaces found! Exiting!')
+            return
+
+        camera = system.cameras[0]
+        event_handler = ImageHandler('./images')
+        camera.init()
+
+        camera.configure_trigger()
+        camera.ExposureAuto.SetValue(PySpin.ExposureAuto_Continuous)
+        camera.configure_acquisition_mode(PySpin.AcquisitionMode_Continuous)
+        # camera.register_event_handler(event_handler)
+        window = gui.launch_gui(camera)
+
+        camera.deinit()
+        del camera
+
+
+
+def wait_for_trigger(event_handler, num_frames=100, wait_time_s=0.1):
+    try:
+        while True:
+            sleep(wait_time_s)
+            if event_handler.num_acquired_images >= num_frames:
+                return
+    except SpinnakerException as se:
+        print('Error while acquiring images!')
+        print(se)
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
