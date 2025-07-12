8338ef9e8776076c176c22cd781cb1334fea1e46
Austin Pauley <austin2118ace@gmail.com>
2025-07-12 19:05:52 -0400

    cleanup __main__; logger for acquisition testing; use trigger_acquisition calls

--- dewan_flir_camera/__main__.py
+++ dewan_flir_camera/__main__.py
@@ -4,10 +4,10 @@ from time import sleep
 from dewan_flir_camera import gui
 from spin_system import SpinSystem
 from acquisition import ImageHandler
-from options import AutoExposureMode, AcquisitionMode
+from options import AutoExposureMode, AcquisitionMode, AcquisitionState
 
 import logging
-logging.basicConfig(level=logging.NOTSET)
+logging.basicConfig(level=logging.DEBUG)
 
 def main():
     logger = logging.getLogger(__name__)
@@ -16,35 +16,29 @@ def main():
         camera = system.cameras[0]
         event_handler = ImageHandler('./images')
         camera.init()
-
         camera.configure_hardware_trigger()
-
-        # camera.register_event_handler(event_handler)
         camera.ExposureAuto.SetValue(AutoExposureMode.CONTINUOUS)
         camera.set_acquisition_mode(AcquisitionMode.CONTINUOUS)
         camera.register_event_handler(event_handler)
-        ui = gui.launch_gui(camera)
+        ui = gui.launch_gui(camera, logger)
 
         while True:
             try:
-                print("Waiting for trigger!")
-                camera.BeginAcquisition()
+                logger.info("Waiting for trigger!")
+                camera.trigger_acquisition(AcquisitionState.BEGIN)
                 event_handler.reset()
-                wait_for_trigger(event_handler)
-                camera.EndAcquisition()
+                wait_for_trigger(event_handler, logger)
+                camera.trigger_acquisition(AcquisitionState.END)
             except KeyboardInterrupt:
-                camera.EndAcquisition()
+                camera.trigger_acquisition(AcquisitionState.END)
+                event_handler.reset()
                 break
-                
-        window = gui.launch_gui(camera)
-        camera.deinit()
-        del camera
-        ui = gui.launch_gui(camera, logger)
 
 
-def wait_for_trigger(event_handler, num_frames=100, wait_time_s=0.1):
+def wait_for_trigger(event_handler, logger, num_frames=100, wait_time_s=0.1):
     try:
         while True:
+            logger.info("%s frames captured", event_handler.num_acquired_images)
             sleep(wait_time_s)
             if event_handler.num_acquired_images >= num_frames:
                 return
