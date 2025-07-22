d54d519b63c326b1c48dcf3e4e6f5f3a3fd53704
Austin Pauley <austin2118ace@gmail.com>
2025-07-21 22:29:06 -0400

    emit Signal on image event with image as an ndarray

--- dewan_flir_camera/acquisition.py
+++ dewan_flir_camera/acquisition.py
@@ -2,17 +2,22 @@ from pathlib import Path
 from typing import Union
 
 import PySpin
+import numpy as np
 from PySpin import ImageEventHandler, ImageProcessor, SpinnakerException
-
+from PySide6.QtCore import Signal, QObject
 
 class ImageHandler(ImageEventHandler):
+    class ImageEventEmitter(QObject):
+        image_event_signal = Signal(np.ndarray)
+        def __init__(self):
+            super().__init__()
     def __init__(self, save_dir: Union[str, Path], logger):
         super().__init__()
         self.logger = logger
         self.save_dir = save_dir
         self.acquired_images = 0
         self._image_processor: PySpin.ImageProcessor = []
-
+        self.image_event_emitter = self.ImageEventEmitter()
         self._init_image_processor()
         self._init_save_dir()
 
@@ -32,12 +37,15 @@ class ImageHandler(ImageEventHandler):
         else:
             try:
                 _image = self._image_processor.Convert(image, PySpin.PixelFormat_Mono8)
-                _filename = f"image-{self.num_acquired_images}.jpg"
-                _file_path = self.save_dir.joinpath(_filename)
-                _image.Save(str(_file_path))
-
+                _image_to_display = _image.GetNDArray()
+                self.image_event_emitter.image_event_signal.emit(_image_to_display)
+                #
+                # _filename = f"image-{self.num_acquired_images}.jpg"
+                # _file_path = self.save_dir.joinpath(_filename)
+                # _image.Save(str(_file_path))
                 # print(f'Saved image: {_file_path}')
                 self.acquired_images += 1
+                _image.Release()
             except Exception as se:
                 self.logger("Error saving image")
 
