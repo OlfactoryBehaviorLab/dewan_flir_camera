669759e44f1ee436ff2ef401b60b396be0971b34
Austin Pauley <austin2118ace@gmail.com>
2025-07-28 16:10:03 -0400

    add some flags to the image handler to turn some features on and off

--- dewan_flir_camera/acquisition.py
+++ dewan_flir_camera/acquisition.py
@@ -16,6 +16,12 @@ class ImageHandler(ImageEventHandler):
         self.logger = logger
         self.save_dir = save_dir
         self.acquired_images = 0
+
+        self.do_save = True
+        self.display = True
+        self.record = True
+
+        self.video_processor = None
         self._image_processor: PySpin.ImageProcessor = []
         self.image_event_emitter = self.ImageEventEmitter()
         self._init_image_processor()
@@ -38,14 +44,22 @@ class ImageHandler(ImageEventHandler):
             try:
                 _image = self._image_processor.Convert(image, PySpin.PixelFormat_Mono8)
                 _image_to_display = _image.GetNDArray()
-                self.image_event_emitter.image_event_signal.emit(_image_to_display)
-                _filename = f"image-{self.num_acquired_images}.jpg"
-                _file_path = self.save_dir.joinpath(_filename)
-                _image.Save(str(_file_path))
+                if self.do_save:
+                    self.save_image(_image)
+                if self.display:
+                    self.image_event_emitter.image_event_signal.emit(_image_to_display)
+                if self.record:
+                    pass
+
                 self.acquired_images += 1
                 _image.Release()
             except Exception as se:
                 self.logger("Error saving image")
+
+    def save_image(self, image):
+        _filename = f"image-{self.num_acquired_images}.jpg"
+        _file_path = self.save_dir.joinpath(_filename)
+        image.Save(str(_file_path))
 
     def reset(self):
         self.acquired_images = 0
