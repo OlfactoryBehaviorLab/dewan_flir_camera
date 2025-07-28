f1a5e247c2c401b268ebbfd8db863dcdaa3c3f2d
Austin Pauley <austin2118ace@gmail.com>
2025-07-28 18:22:52 -0400

    add video type enum to options

--- dewan_flir_camera/options.py
+++ dewan_flir_camera/options.py
@@ -17,3 +17,11 @@ class AutoExposureMode(IntEnum):
     OFF = PySpin.ExposureAuto_Off  # 0
     ONCE = PySpin.ExposureAuto_Once  # 1
     CONTINUOUS = PySpin.ExposureAuto_Continuous  # 2
+
+
+class VideoType:
+    """'Enum' to select video type to be created and saved"""
+    UNCOMPRESSED = 0
+    MJPG = 1
+    H264_AVI = 2
+    H264_MP4 = 3
