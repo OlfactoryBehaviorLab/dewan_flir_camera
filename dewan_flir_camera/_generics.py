3d8483317cd194425b236abef324fad424d7ccad
Austin Pauley <austin2118ace@gmail.com>
2025-10-28 19:31:19 -0400

    _generics.py cleanup SpinnakerObject

--- dewan_flir_camera/_generics.py
+++ dewan_flir_camera/_generics.py
@@ -1,10 +1,15 @@
+import logging
 import PySpin
 
+logger = logging.getLogger(__name__)
 
 class SpinnakerObject:
-    def __init__(self, ptr, logger):
+    """ Parent class for all Spinnaker objects
+    Holds reference to pointer that all spinnaker objects will have and some simple dunder methods
+
+    """
+    def __init__(self, ptr):
         self.ptr = ptr
-        self.logger = logger
 
     def __enter__(self):
         return self
@@ -14,20 +19,31 @@ class SpinnakerObject:
             # If we got here and the pointer is still around; release it
             self.deinit()
 
-        import traceback
-
         if exc_type is not None:
-            traceback.print_exception(exc_type, exc_val, exc_tb)
+            logger.error("SpinnakerObject error!", exc_info=(exc_type, exc_val, exc_tb))
 
     def __repr__(self):
         return f"Class: {self.__class__}"
 
     def deinit(self):
-        self.logger.debug("Deleting %s", self.ptr)
+        logger.debug("Deleting %s", self.ptr)
         del self.ptr
 
     @staticmethod
-    def get_node_info(node):
+    def get_node_info(node) -> str | None:
+        """ Will check that a Spinnaker node exists and is readable
+
+        Parameters
+        ----------
+        node
+            Spinnaker object node to return value of
+
+        Returns
+        -------
+        str | None
+            Returns node represented as a string or None if it is unreadable or doesn't exist
+
+        """
         if node is not None and PySpin.IsReadable(node):
             return PySpin.CValuePtr(node).ToString()
         else:
